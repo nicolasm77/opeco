4be6261282bf6b85f1cefaa4c37ca619db7eb74f
Maxime Blanquart <maxime.blanquart@gmail.com>
2018-09-21 17:32:28 +0200

    JS

--- evenements/equipement-maison/scripts/onload.js
+++ evenements/equipement-maison/scripts/onload.js
@@ -1,5 +1,48 @@
-function BcleanHasTag(a) {
-	return a.split("#")[1];
+function designLavaBar(origin){
+	var selected = $j('.b-nav__link.selected');
+	if(origin === undefined){
+		origin = {};
+		origin.pos = selected.position().left;
+		origin.color = selected.attr("rel");
+		origin.width = selected.width();
+	}
+	$j(".b-underline").css({
+		left: origin.pos,
+		width: origin.width,
+		backgroundColor: origin.color
+	});
+}
+function toggleLavaBar(element, status) {
+	var lavaBar = $j(".b-underline");
+	if (element !== undefined) {
+		if (status === 'out') {
+			var selected = $j('.b-nav__link.selected');
+			origin = {};
+			origin.pos = selected.position().left;
+			origin.color = selected.attr("rel");
+			origin.width = selected.width();
+			lavaBar.stop().animate({
+				left: origin.pos,
+				width: origin.width,
+				backgroundColor:origin.color
+			});
+		} else {
+			lavaBar.width($j(".selected").parent().width())
+				.css("left", origin["pos"])
+				.data("origLeft", origin["pos"])
+				.data("origWidth", origin["width"])
+				.data("origColor", origin["color"]);
+			if (element !== undefined) {
+				lavaBar.stop().animate({
+					left: element.position().left,
+					width: element.width(),
+					backgroundColor: element.attr("rel")
+				});
+			}
+		}
+	} else {
+		designLavaBar();
+	}
 }
 function Binject(a) {
 	var url = "ajax-" + a + ".html";
@@ -9,42 +52,26 @@ function Binject(a) {
 	$j.ajax({
 		url: url,
 		success: function (data) {
-			var $el, leftPos, newWidth, $magicLine = $j(".b-underline");
 			$j('.b-content').html(data);
 			$j('.page-evenement a.selected').removeClass('selected');
 			$j('a[href=#' + a + ']').addClass('selected');
-
-			$magicLine.width($j(".selected").parent().width())
-			.css("left", $j(".selected").position().left)
-			.data("origLeft", $magicLine.position().left)
-			.data("origWidth", $magicLine.width())
-			.data("origColor", $j(".selected").attr("rel"));
-			$j(".b-nav__list a").hover(function() {
-				$el = $j(this);
-				leftPos = $el.position().left;
-				newWidth = $el.width();
-				$magicLine.stop().animate({
-					left: leftPos,
-					width: newWidth,
-					backgroundColor: $el.attr("rel")
-				});
-			}, function() {
-				$magicLine.stop().animate({
-					left: $magicLine.data("origLeft"),
-					width: $magicLine.data("origWidth"),
-					backgroundColor: $magicLine.data("origColor")
-				});
+			$j(".b-nav__link:not(.selected)").hover(function () {
+				toggleLavaBar($j(this));
+			},function () {
+				toggleLavaBar($j(this), 'out');
 			});
-			$j(".b-nav__list a.selected").trigger('mouseover');
-
+			setTimeout(function () {
+				toggleLavaBar();
+			}, 50);
 		}
 	});
 }
 function BchangeURL() {
 	var hashTag = window.location.hash;
 	if (hashTag) {
-		if (BcleanHasTag(hashTag) === "cuisine" || BcleanHasTag(hashTag) === "linge-maison") {
-			Binject(BcleanHasTag(hashTag));
+		var b = hashTag.split("#")[1];
+		if (b === "cuisine" || b === "linge-maison") {
+			Binject(b);
 		}
 	} else {
 		Binject('cuisine');
@@ -55,5 +82,4 @@ $j(window).on('hashchange', function () {
 });
 $j(function () {
 	$j(".b-nav__list").append("<span class='b-underline'></span>");
-	BchangeURL();
 }).trigger('hashchange');
