c27a17862ae474eb739372e2ba320283ef4248dd
nicolasm77 <40019132+nicolasm77@users.noreply.github.com>
2018-10-30 17:30:23 +0100

    NOEL - app + menu + video

--- evenements/noel-2018/index.js
+++ evenements/noel-2018/index.js
@@ -3,6 +3,10 @@ import bubblecss from "bubbles/dist/main.css";
 import bubbleCalcul from "bubbles/dist/main.js";
 import bubbleAnim from "bubbles/dist/anims.js";
 
+//custom scrollbar
+import 'simplebar'; // or "import SimpleBar from 'simplebar';" if you want to use it manually.
+import 'simplebar/dist/simplebar.css';
+
 //fichier CSS principal
 import style from "./styles/style.scss";
 
@@ -23,6 +27,7 @@ window.lazySizesConfig.srcAttr = 'data-lazy';
 window.lazySizesConfig.expand = 350;
 
 
+
 $j(document).ready(function() {
 	$j(".advent__calendar--decembre .advent__calendar-month").on("click", function(){
 		$j(".advent__calendar--decembre").addClass("advent__calendar--open")
@@ -30,5 +35,10 @@ $j(document).ready(function() {
 
 	$j(".advent__calendar--novembre .advent__calendar-month").on("click", function(){
 		$j(".advent__calendar--decembre").removeClass("advent__calendar--open")
+	})
+
+	$j(".video__item").on("click", function(){
+		$j(".video__item.current").removeClass("current");
+		$j(this).addClass("current")
 	})
 });
